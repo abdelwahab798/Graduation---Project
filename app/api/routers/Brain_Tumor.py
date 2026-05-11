ec3006c82b5ab1fdf9e7a432c43a019198625ff9
Abdelwahab <abdelwahabamr769@gmail.com>
2026-05-11 22:53:27 +0300

    add Brain model in fastapi

---
+++ app/api/routers/Brain_Tumor.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter, File, UploadFile, HTTPException
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+from PIL import Image
+import io
+
+router = APIRouter(
+    prefix="/Brain_MRI",
+    tags=["Brain Tumor Analysis"]
+)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class_names = ['meningioma_tumor', 'no_tumor', 'glioma_tumor','pituitary_tumor']
+
+def load_model():
+    path = "models/resnet18_brain_tumor.pth"
+    model = models.resnet18()
+    in_features = model.fc.in_features
+    model.fc = nn.Sequential(
+        nn.BatchNorm1d(in_features),
+        nn.Dropout(p=0.5),
+        nn.Linear(in_features, 4)
+    )
+    
+    state_dict = torch.load(path, map_location=device)
+    model.load_state_dict(state_dict)
+    model.to(device)
+    model.eval()
+    return model
+
+model = load_model()
+
+preprocess = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+@router.post("/predict")
+async def predict_brain_tumor(file: UploadFile = File(...)):
+    try:
+        image_data = await file.read()
+        image = Image.open(io.BytesIO(image_data)).convert('RGB')
+        input_tensor = preprocess(image).unsqueeze(0).to(device)
+        with torch.no_grad():
+            outputs = model(input_tensor)
+            probabilities = torch.nn.functional.softmax(outputs[0], dim=0)
+            conf, preds = torch.max(probabilities, dim=0)
+        
+        return {
+            "filename": file.filename,
+            "prediction": class_names[preds.item()],
+            "confidence": round(conf.item(), 4),
+            "all_probabilities": {class_names[i]: round(probabilities[i].item(), 4) for i in range(len(class_names))},
+            "status": "success"
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from api.routers import chest_xray
+from api.routers import Brain_Tumor
 app = FastAPI(title="MediScan AI Platform")
 
 app.include_router(chest_xray.router)
+app.include_router(Brain_Tumor.router)
 
 @app.get("/")
 def home():
